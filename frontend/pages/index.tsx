e881ce83523bc8a6147a03be5a81075af4c0cf4a
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-06-14 18:48:17 -0700

    shell

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+import type { NextPage } from 'next';
+import React from 'react';
+import DLCAppShell from '../components/navigation/DLCAppShell';
+
+const Home: NextPage = () => {
+  return (
+    <DLCAppShell>
+    A
+    </DLCAppShell>
+  );
+};
+
+export default Home;
+
