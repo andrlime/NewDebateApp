220e41f1ff19b2c3e28ac716ed3ec6b1341e2882
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-08-02 20:56:22 -0400

    working frontend :)

--- frontend/lib/roots/ViewParadigms.tsx
+++ frontend/lib/roots/ViewParadigms.tsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { IJudge } from "../interfaces";
+import { RootState } from "../store/reducers/reduce";
+import judgeStyles from './judge.module.css';
+import { Table, TextInput, Title } from "@mantine/core";
+import parse from 'html-react-parser';
+import axios from "axios";
 
 export const ViewParadigms: React.FC = () => {
-    return (
-        <></>
+    const backendUrl = useSelector((state: RootState) => state.env.backendUrl);
+    const [judgeList, setJudgeList] = useState<IJudge[]>();
+    const [activeJudge, setActiveJudge] = useState<IJudge | null>();
+
+    useEffect(() => {
+        axios.get(`${backendUrl}/get/judges`)
+            .then(res => {
+                if(res.status != 200) return;
+
+                setJudgeList(res.data);
+            })
+    }, [backendUrl]);
+    
+    const [filter, setFilter] = useState("");
+
+    const leftComponent = (
+        <div style={{display: "flex", flexDirection: "row"}}>
+            <div style={{width: "50%"}}>
+                <TextInput value={filter} onChange={(e) => setFilter(e.target.value)} placeholder="search" radius="xl" py='sm' />
+                <Table striped highlightOnHover withBorder withColumnBorders>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody className={judgeStyles.judgeTableBody}>
+                        {judgeList && judgeList.filter(a => a.name.toLowerCase().includes(filter.toLowerCase())).map((judge, index) => (
+                            <tr onClick={() => setActiveJudge(judge)} className={judgeStyles.row} key={`judge-db-${index}`}>
+                                <td>{judge.name}</td>
+                            </tr>
+                        )) || <tr>
+                                <td>Loading</td>
+                            </tr>}
+                    </tbody>
+                </Table>
+            </div>
+            <div style={{width: "50%", height: "100%", padding: "1rem"}}>
+                {activeJudge && <div>
+                    <Title order={4}>{activeJudge?.name}'s Paradigm</Title>
+                    {parse(activeJudge.paradigm)}
+                    </div>}
+            </div>
+        </div>
     );
+    
+    return leftComponent;
 };
 
 export default ViewParadigms;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,11 +2,12 @@
 import type { NextPage } from 'next';
 import React from 'react';
 import DLCAppShell from '../lib/components/DLCAppShell';
+import ViewParadigms from '../lib/roots/ViewParadigms';
 
 const Home: NextPage = () => {
   return (
     <DLCAppShell active_index={0}>
-      
+      <ViewParadigms />
     </DLCAppShell>
   );
 };
